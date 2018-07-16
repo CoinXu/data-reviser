dacc7862914a361fab33a36f0f5988a02cd58a7e
zhuoyihan <zhuoyihan@yy.com>
2018-07-16 15:07:58 +0800

    export staticdata

--- src/script/StaticData.ts
+++ src/script/StaticData.ts
@@ -1,5 +1,4 @@
 export const __D_NAME__ = "_validator_";
-export const __Array_tag__ = "_isArray_";
 export const __CLASS_TYPE_ = "_class_";
 export const __ERROR_TYPE__ = {
   type_error: "type error",
--- src/script/index.js
+++ src/script/index.js
@@ -1,9 +1,11 @@
 import {Decorator} from "../impl/Decorator";
 import {structType} from "../impl/structType";
 import {Validator} from "../impl/Validator";
+import {StaticData} from "../script/StaticData";
 
 export {
   Decorator,
   structType,
-  Validator
+  Validator,
+  StaticData
 }
